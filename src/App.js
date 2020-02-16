725dab209490018230de5296e056bac06316b890
Paulo Ricardo Teixeira <paulo.dsn.cir@alterdata.com.br>
2020-02-16 19:26:36 -0300

    atualização automatica da tabelao

--- src/App.js
+++ src/App.js
@@ -12,87 +12,100 @@ import ServiceApi from './useful/ServiceApi'
 
 function App() {
   const [state, setState] = useState(
-   { card:[
-     
     {
-      value: '997',
-      color: '#a08365',
-      type: 'mail',
-      header: 'Total'
-    },
-    {
-      value: '1457',
-      color: '#8a6dd5',
-      type: 'clock-circle',
-      header: 'Aguardando Envio'
-    },
-    {
-      value: '431',
-      color: '#22A7F0',
-      iconClass: 'fa fa-paper-plane-o',
-      header: 'Enviados'
-    },
-    {
-      value: '35',
-      color: '#1ABC9C',
-      type: 'check',
-      header: 'Entregues'
-    },
-    {
-      value: '342',
-      color: '#FF0000',
-      iconClass: 'fa fa-exclamation-triangle',
-      header: 'Bounce Sofridos'
-    },
-    {
-      value: '350',
-      color: '#ffb400',
-      type: 'alert',
-      header: 'Bounce Evitados'
+      card: [
+
+        {
+          value: '997',
+          color: '#a08365',
+          type: 'mail',
+          header: 'Total'
+        },
+        {
+          value: '1457',
+          color: '#8a6dd5',
+          type: 'clock-circle',
+          header: 'Aguardando Envio'
+        },
+        {
+          value: '431',
+          color: '#22A7F0',
+          iconClass: 'fa fa-paper-plane-o',
+          header: 'Enviados'
+        },
+        {
+          value: '35',
+          color: '#1ABC9C',
+          type: 'check',
+          header: 'Entregues'
+        },
+        {
+          value: '342',
+          color: '#FF0000',
+          iconClass: 'fa fa-exclamation-triangle',
+          header: 'Bounce Sofridos'
+        },
+        {
+          value: '350',
+          color: '#ffb400',
+          type: 'alert',
+          header: 'Bounce Evitados'
+        }
+
+      ],
+
+
+      Table: [{
+        ap_id: 1,
+        ap_name: 'Karoo',
+        total: '32',
+        waitSend: '4',
+        waitDelivery: '4',
+        Delivered: '24',
+        bounce: '0',
+        complaint: '9'
+      }]
+
     }
-
-    ] ,     
-    
-
-    Table:[ {
-      ap_id: 1,
-      ap_name: 'Karoo',
-      total: '32',
-      waitSend: '4',
-      waitDelivery: '4',
-      Delivered: '24',
-      bounce: '0',
-      complaint: '9'
-    }]
-
-   }
   );
 
 
 
 
 
-  async function fechData() {
+  async function fechDataCard() {
 
     const responseCard = await PrepareCardData();
-    let copyState=state.Table 
-    let ajust=({ 
-        card:responseCard.card,
-        Table:copyState })
+    let copyState = state.Table
+    let ajust = ({
+      card: responseCard.card,
+      Table: copyState
+    })
 
     setState(ajust);
   }
-   
-    
-    
-
-
-   // const  responseTable = await ServiceApi.getEstatisticasPorApp();
-
-  //  setState (responseTable)
- 
 
   
+  async function fechDataTable(){
+     
+     const  responseTable = await ServiceApi.getEstatisticasPorApp();
+
+     console.log('responsetable',responseTable)
+     let copyState = state.card
+     let ajust = ({
+       card:copyState,
+       Table: responseTable.data
+     })
+ 
+     setState(ajust);
+   }
+ 
+  
+
+
+
+
+
 
 
 
@@ -106,7 +119,8 @@ function App() {
         ))}
         tabela={<TableStyle dataValue={state.Table} />}
       />
-      <button onClick={() => { fechData() }} >Atualizar</button>
+      <button onClick={() => { fechDataCard() }} >Atualizar card</button>
+      <button onClick={() => { fechDataTable() }} >Atualizar tabela</button>
     </Fragment>
   );
 }
